532d14fd8b39c44b1573ae330b8e7520a8d5bebc
Alvaro Paçó <alvaro@async.com.br>
2014-03-08 00:43:54 -0300

    Configuring phpAR ORM

--- app/admin/controller/common/commonController.php
+++ app/admin/controller/common/commonController.php
@@ -10,6 +10,34 @@ class common extends Controller{
 
 	public function index(){
 		echo "Hello World!";
+		# create Tito
+		/*$user = Users::create(array(
+			'username' => 'Tito',
+		 	'password' => sha1("alabesco"),
+		 	'group_id' => '1',
+		 	'state' => '1')
+		);*/
+		
+		$user = new User();
+		$user->username = 'Tito';
+		$user->password = sha1("alabesco");
+		$user->group_id = '1';
+		$user->state 	= '1';
+		try{
+			$user->save();
+		}catch(Exception $e){
+			//echo 'Message: ' .$e->getMessage();
+		}
+
+		# read Tito
+		$user = User::find_by_username('Tito');
+		echo $user->password;
+		# update Tito
+		# $user->name = 'Tito Jr';
+		# $user->save();
+	 
+		# delete Tito
+		#$user->delete();
 	}
 }
 
--- mysql_alter_tables.sql
+++ mysql_alter_tables.sql
@@ -1,3 +1,11 @@
 # Add a constraint to index state to cities and states to countries
 ALTER TABLE `cities` ADD CONSTRAINT `fk_state` FOREIGN KEY ( `zone_id` ) REFERENCES `states` ( `zone_id` ) ;
 ALTER TABLE `states` ADD CONSTRAINT `fk_country` FOREIGN KEY ( `zone_country_id` ) REFERENCES `countries` ( `countries_id` ) ;
+
+# Alter Engine 
+ALTER TABLE cities ENGINE=InnoDB;
+ALTER TABLE states ENGINE=InnoDB;
+ALTER TABLE countries ENGINE=InnoDB;
+
+# Table users with utf8
+ALTER TABLE users DEFAULT CHARACTER SET utf8;
